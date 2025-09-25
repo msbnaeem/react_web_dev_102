ac2617126255016918f53541b4df4c145fdd02f0
Mohammad Naeem <binnaeem96@gmail.com>
2025-09-25 01:03:44 -0400

    adding lab1 to repo

---
+++ timetabled_lab1/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import Calendar from './components/Calendar';
+import Event from './components/Event';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Week on Cruise Ship</h1>
+      <h2>Let's go on cruise for vacation!</h2>
+      <Calendar />
+      
+
+
+      
+    </div>
+  )
+}
+
+export default App
---
+++ timetabled_lab1/src/components/Event.jsx
@@ -0,0 +1,16 @@
+import '../index.css'
+import React from "react";
+
+const Event = (props) => {
+
+    return (
+        <td className="Event">
+            <h5>{props.event}</h5>
+            <h6>{props.location}</h6>
+        </td>
+    )
+    
+  
+}
+
+export default Event;
