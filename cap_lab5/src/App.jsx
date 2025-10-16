ff562a6561e00c3ff3f03920790040b300e4fbf0
Mohammad Naeem <binnaeem96@gmail.com>
2025-10-15 20:46:40 -0400

    cap lab initial commit

---
+++ cap_lab5/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
