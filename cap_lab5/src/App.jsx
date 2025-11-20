e43c4c2ab8607265f7fd4fe6a1b6024c1fa8ea2b
Mohammad Naeem <binnaeem96@gmail.com>
2025-11-19 22:43:37 -0500

    adding

--- cap_lab5/src/App.jsx
+++ cap_lab5/src/App.jsx
@@ -1,15 +1,42 @@
 import { useState } from 'react'
 const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
 import './App.css'
+import { APIForm } from './component/APIForm';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputs, setInputs] = useState({
+    url: "",
+    format: "",
+    no_ads: "",
+    no_cookie_banners: "",
+    width: "",
+    height: "",
+  });
 
+  const submitForm = () => {
+ 
+
+  }
+  
   return (
-    <>
+    <div className="whole-page">
+      <h1>Build Your Own Screenshot! 📸</h1>
       
-    </>
-  )
+      <APIForm
+        inputs={inputs}
+        handleChange={(e) =>
+          setInputs((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value.trim(),
+          }))
+        }
+        onSubmit={submitForm}
+      />
+      <br></br>
+
+    </div>
+  );
 }
 
 export default App
